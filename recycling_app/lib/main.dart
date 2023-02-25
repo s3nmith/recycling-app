bedc59286a2aff84fd5b06e839d6e30fbac8ef67
Jimmy <alphinewhite4559@gmail.com>
2023-02-25 17:19:08 +0900

    Update main.dart
    removed one Korean comment

--- recycling_app/lib/main.dart
+++ recycling_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(), // 홈페이지 보여주기
+      home: HomePage(),
     );
   }
 }
