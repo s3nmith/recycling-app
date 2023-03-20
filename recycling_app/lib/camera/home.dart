5a92e6760daafa4e71d045f311bf52c23bfd9fcd
krhdh <kkaneda19@gmail.com>
2023-03-20 23:24:54 +0900

    Fixed an issue where clicking "Analyze" before selecting the image would crash the app

--- recycling_app/lib/camera/home.dart
+++ recycling_app/lib/camera/home.dart
@@ -99,199 +99,181 @@ class _HomeState extends State<CameraScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              _image != null
-                  ? Column(
-                      children: [
-                        imagePicked == false
-                            ? Column(children: [
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: SizedBox(
-                                      height: 175,
-                                      child: Image.asset(
-                                          'assets/images/decorations/ume.png')),
-                                ),
-                                Text(
-                                  'Please select an image to proceed.',
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+              Column(
+                children: [
+                  modelRun == true
+                      ? Column(children: [
+                          _output == null
+                              ? Column(
+                                  children: [
+                                    Text('Waiting for classification.'),
+                                  ],
                                 )
-                              ])
-                            : Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(
-                                        top: 5,
-                                        bottom: 5,
-                                        left: 8.0,
-                                        right: 8.0),
-                                    child: Image.file(
-                                      _image,
-                                      height: 336,
-                                      width: 252,
-                                    ),
-                                  ),
-                                  Column(
-                                    children: [
-                                      modelRun == true
-                                          ? Column(children: [
-                                              _output == null
-                                                  ? Text(
-                                                      'Waiting for classification.')
-                                                  : Column(
-                                                      children: [
-                                                        Text(
-                                                          'The trash type is:',
-                                                          textAlign:
-                                                              TextAlign.center,
-                                                          style: TextStyle(
-                                                            color: Colors.black,
-                                                            fontSize: 18,
-                                                            fontWeight:
-                                                                FontWeight.w400,
-                                                          ),
-                                                        ),
-                                                        SizedBox(height: 10),
-                                                        Text(
-                                                          '${_output?[0]['label'] ?? ''}',
-                                                          textAlign:
-                                                              TextAlign.center,
-                                                          style: TextStyle(
-                                                            color: Colors.black,
-                                                            fontSize: 18,
-                                                            fontWeight:
-                                                                FontWeight.w400,
-                                                          ),
-                                                        ),
-                                                        SizedBox(height: 10),
-                                                        Text(
-                                                          'Trash category in "" is ""',
-                                                          style: TextStyle(
-                                                            fontSize: 16,
-                                                          ),
-                                                        ),
-                                                      ],
-                                                    )
-                                            ])
-                                          : Column(
-                                              children: [
-                                                analyzing == false
-                                                    ? Container()
-                                                    : CircularProgressIndicator()
-                                              ],
-                                            )
-                                    ],
-                                  )
-                                ],
-                              ),
-                        Column(children: [
-                          Column(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    top: 10, bottom: 10.0),
-                                child: SizedBox(
-                                  height: 55,
-                                  width: 380,
-                                  child: ElevatedButton.icon(
-                                    icon: const Icon(
-                                      Icons.analytics_outlined,
-                                      color: Colors.white,
-                                      size: 40,
-                                    ),
-                                    label: const Text('               Analyze',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w500)),
-                                    style: ElevatedButton.styleFrom(
-                                        shape: RoundedRectangleBorder(
-                                            borderRadius:
-                                                BorderRadius.circular(10.0)),
-                                        primary: Colors.blue,
-                                        onPrimary: Colors.white,
-                                        textStyle:
-                                            const TextStyle(fontSize: 18),
-                                        padding: EdgeInsets.only(right: 100)),
-                                    onPressed: () {
-                                      analyzing = true;
-                                      classifyButtonPressed();
-                                    },
-                                  ),
-                                ),
-                              ),
-                              Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  SizedBox(
-                                    height: 55,
-                                    width: 380,
-                                    child: ElevatedButton.icon(
-                                      icon: const Icon(
-                                        Icons.camera_alt_outlined,
-                                        color: Colors.white,
-                                        size: 40,
+                              : Column(
+                                  children: [
+                                    Text(
+                                      'The trash type is:',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
                                       ),
-                                      label: const Text(
-                                          '            Take Picture',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.w500)),
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.only(right: 75),
-                                          primary: Colors.green,
-                                          onPrimary: Colors.white,
-                                          //shadowColor: Colors.blue,
-                                          //elevation: 2,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(10.0)),
-                                          textStyle:
-                                              const TextStyle(fontSize: 18)),
-                                      onPressed: () {
-                                        pickImage(ImageSource.camera);
-                                      },
                                     ),
-                                  ),
-                                  SizedBox(height: 10),
-                                  SizedBox(
-                                    height: 55,
-                                    width: 380,
-                                    child: ElevatedButton.icon(
-                                      icon: const Icon(
-                                        Icons.photo_album_outlined,
-                                        color: Colors.white,
-                                        size: 40,
+                                    SizedBox(height: 10),
+                                    Text(
+                                      '${_output?[0]['label'] ?? ''}',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w400,
                                       ),
-                                      label: const Text(
-                                        '  Select from Gallery',
-                                        style: TextStyle(
-                                            fontWeight: FontWeight.w500),
-                                        textAlign: TextAlign.center,
-                                      ),
-                                      style: ElevatedButton.styleFrom(
-                                          padding: EdgeInsets.only(right: 39),
-                                          primary: Colors.orange,
-                                          onPrimary: Colors.white,
-                                          //shadowColor: Colors.blue,
-                                          //elevation: 2,
-                                          shape: RoundedRectangleBorder(
-                                              borderRadius:
-                                                  BorderRadius.circular(10.0)),
-                                          textStyle:
-                                              const TextStyle(fontSize: 18)),
-                                      onPressed: () {
-                                        pickImage(ImageSource.gallery);
-                                      },
                                     ),
-                                  ),
-                                ],
-                              ),
-                            ],
+                                    SizedBox(height: 10),
+                                    Text(
+                                      'Trash category in "" is ""',
+                                      style: TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  ],
+                                )
+                        ])
+                      : Column(
+                          children: [
+                            analyzing == false
+                                ? Container()
+                                : CircularProgressIndicator()
+                          ],
+                        )
+                ],
+              ),
+              Column(
+                children: [
+                  imagePicked == false
+                      ? Column(children: [
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: SizedBox(
+                                height: 175,
+                                child: Image.asset(
+                                    'assets/images/decorations/ume.png')),
+                          ),
+                          Text(
+                            'Please select an image to proceed.',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ])
+                      : Column(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  top: 5, bottom: 5, left: 8.0, right: 8.0),
+                              child: Image.file(
+                                _image,
+                                height: 336,
+                                width: 252,
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(top: 10, bottom: 10.0),
+                              child: SizedBox(
+                                height: 55,
+                                width: 380,
+                                child: ElevatedButton.icon(
+                                  icon: const Icon(
+                                    Icons.analytics_outlined,
+                                    color: Colors.white,
+                                    size: 40,
+                                  ),
+                                  label: const Text('               Analyze',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w500)),
+                                  style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0)),
+                                      primary: Colors.blue,
+                                      onPrimary: Colors.white,
+                                      textStyle: const TextStyle(fontSize: 18),
+                                      padding: EdgeInsets.only(right: 100)),
+                                  onPressed: () {
+                                    analyzing = true;
+                                    classifyButtonPressed();
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                  Column(children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 55,
+                          width: 380,
+                          child: ElevatedButton.icon(
+                            icon: const Icon(
+                              Icons.camera_alt_outlined,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                            label: const Text('            Take Picture',
+                                style: TextStyle(fontWeight: FontWeight.w500)),
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.only(right: 75),
+                                primary: Colors.green,
+                                onPrimary: Colors.white,
+                                //shadowColor: Colors.blue,
+                                //elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                textStyle: const TextStyle(fontSize: 18)),
+                            onPressed: () {
+                              pickImage(ImageSource.camera);
+                            },
+                          ),
+                        ),
+                        SizedBox(height: 10),
+                        SizedBox(
+                          height: 55,
+                          width: 380,
+                          child: ElevatedButton.icon(
+                            icon: const Icon(
+                              Icons.photo_album_outlined,
+                              color: Colors.white,
+                              size: 40,
+                            ),
+                            label: const Text(
+                              '  Select from Gallery',
+                              style: TextStyle(fontWeight: FontWeight.w500),
+                              textAlign: TextAlign.center,
+                            ),
+                            style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.only(right: 39),
+                                primary: Colors.orange,
+                                onPrimary: Colors.white,
+                                //shadowColor: Colors.blue,
+                                //elevation: 2,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                textStyle: const TextStyle(fontSize: 18)),
+                            onPressed: () {
+                              pickImage(ImageSource.gallery);
+                            },
+                          ),
+                        ),
                       ],
-                    )
-                  : Text('Error loading the app.')
+                    ),
+                  ])
+                ],
+              )
             ],
           ),
         ));
